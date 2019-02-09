bf2174d4293c12ea2fd9cf1b837e299d5073a7b6
Adam Polak <polakadam@outlook.com>
2019-02-09 12:59:26 +0100

    Publish command bus

--- build/index.js
+++ build/index.js
@@ -5,3 +5,4 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./logger"));
 __export(require("./tracer"));
+__export(require("./command-bus"));
