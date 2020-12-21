a1224dd32a7c8a547b389e41840efee4c61d95f9
Marcin Kopa <marcin.info@gmail.com>
2020-12-21 12:59:12 +0100

    Add obfuscation

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,24 +1,52 @@
 import * as winston from "winston";
-import { Logger } from "./logger.types";
 
-const logFormat = winston.format.printf(({ level, message, meta }) => {
-  const stack = meta && meta.stack ? meta.stack : undefined;
+export const obfuscateCircular = (obj, keysToHide) => {
+  const seen = new WeakSet();
+  const obfuscatory = "******";
 
-  return JSON.stringify({
-    "@timestamp": new Date().toISOString(),
-    "@version": 1,
-    application: process.env.APP_NAME,
-    environment: process.env.NODE_ENV,
-    host: process.env.HOST,
-    message,
-    stack,
-    severity: level,
-    type: "stdin",
+  return JSON.stringify(obj, (k, v) => {
+    if (keysToHide.includes(k)) {
+      if (v !== null && typeof v === "string" && v.length >= 20) {
+        return `${obfuscatory}${v.substring(v.length - 6)}`;
+      }
+      return obfuscatory;
+    }
+    if (v !== null && typeof v === "object") {
+      if (seen.has(v)) {
+        return undefined;
+      }
+      seen.add(v);
+    }
+    return v;
   });
-});
+};
 
-export const winstonLogger: Logger = winston.createLogger({
-  level: process.env.LOGGING_LEVEL || "debug",
-  format: winston.format.combine(winston.format.splat(), logFormat),
-  transports: [new winston.transports.Console()],
-});
+const logFormat = (
+  env = process.env,
+  keysToHide: string[] = env.LOGGER_KEYS_TO_HIDE ? env.LOGGER_KEYS_TO_HIDE.split(",") : [],
+) =>
+  winston.format.printf(({ level, message, meta }) => {
+    const stack = meta && meta.stack ? meta.stack : undefined;
+
+    const logMessage = typeof message === "object" ? obfuscateCircular(message, keysToHide) : message;
+
+    return JSON.stringify({
+      "@timestamp": new Date().toISOString(),
+      "@version": 1,
+      application: env.APP_NAME,
+      environment: env.NODE_ENV,
+      host: env.HOST,
+      message: logMessage,
+      meta,
+      stack,
+      severity: level,
+      type: "stdin",
+    });
+  });
+
+export const createLogger = (keysToHide: string[], env = process.env) =>
+  winston.createLogger({
+    level: env.LOGGING_LEVEL || "debug",
+    format: winston.format.combine(winston.format.splat(), logFormat(env, keysToHide)),
+    transports: [new winston.transports.Console()],
+  });
--- packages/logger/src/tests/logger.spec.ts
+++ packages/logger/src/tests/logger.spec.ts
@@ -1,3 +1,23 @@
 import "mocha";
+import { createLogger } from "..";
 
-describe("logger", () => {});
+describe("logger", () => {
+  it("Check logger format", async () => {
+    const winstonLogger = createLogger(["secure"]);
+
+    class Circular {
+      constructor() {
+        this.secure = "secure data";
+        this.self = this;
+      }
+
+      private self: Circular;
+
+      private secure: string;
+    }
+
+    const circularObject = new Circular();
+
+    winstonLogger.info(circularObject);
+  });
+});
